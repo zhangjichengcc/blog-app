6ab7dcae5e84385c4ade4d1bb59d113fb99ff4c6
zhangjicheng <zhangjichengcc@163.com>
2021-11-10 18:20:12 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:52:20
- * @LastEditTime: 2021-11-10 16:06:40
+ * @LastEditTime: 2021-11-10 17:50:56
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\app.ts
@@ -11,5 +11,5 @@
 import { history } from 'umi';
 
 export function onRouteChange({ location, routes, action }: any) {
-  console.log(location.pathname);
+  // console.log(location.pathname);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,43 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:30:44
- * @LastEditTime: 2021-11-10 16:34:46
+ * @LastEditTime: 2021-11-10 18:01:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\pages\index.tsx
  */
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import { oauthGithub } from '@/services/user';
+import { history } from 'umi';
 import styles from './index.less';
 
-export default function IndexPage(props) {
+export default function IndexPage(props: any) {
 
   const { location } = props;
   const { query } = location;
 
-  console.log(process.env.ClientID)
-  console.log(process.env.ClientSecret)
+  const [userinfo, setUserInfo] = useState<any>({});
+  const [loading, setLoading] = useState(false);
 
   function login() {
-    // const CLIENT_ID = "Iv1.d0c59d733fa5727d"; // test
     const CLIENT_ID = process.env.ClientID; // prod
-    const REDIRECT_URL = "http://localhost:8800/user/oauth_github";
-    const url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + `&client_secret=${REDIRECT_URL}`;
+    const url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + `&client_secret=${process.env.ClientSecret}`;
     window.location.href = url;
   }
 
   function authLogin() {
-    oauthGithub({code: query.code}).then(res => {
+    const code = query.code;
+    history.replace('/');
+    setLoading(true);
+    oauthGithub({code}).then(res => {
+      setLoading(false);
       console.log(res);
+      if (res.code === 0) {
+        const { data } = res;
+        setUserInfo(data);
+      }
     }).catch(err => {
+      setLoading(false)
       console.log(err);
     })
   }
@@ -42,8 +50,24 @@ export default function IndexPage(props) {
 
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
-      <button onClick={login}>Log in by github</button>
+      { 
+        loading ?
+        <p>loading</p> :
+        <>
+          { userinfo.username ? 
+            <>
+              <img src={userinfo.avatar} />
+              <p>name: <span>{userinfo.username}</span></p>
+              <p>email: <span>{userinfo.email}</span></p>
+              <p>token: <span style={{display: 'inline-block', width: '400px', wordBreak: 'break-all'}}>{userinfo.token}</span></p>
+            </> :
+            <>
+              <h1 className={styles.title}>Page index</h1>
+              <button onClick={login}>Log in by github</button>
+            </>
+          }
+        </>
+      }
     </div>
   );
 }
