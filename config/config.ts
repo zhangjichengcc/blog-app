7431f025bffc6def6ef19f319d9fcc216d9dc194
zhangjicheng <zhangjichengcc@163.com>
2021-12-17 18:13:58 +0800

    init pageLoading

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:46:05
- * @LastEditTime: 2021-12-17 10:42:56
+ * @LastEditTime: 2021-12-17 18:10:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\config\config.ts
@@ -18,7 +18,9 @@ import * as path from 'path';
 // } = process.env;
 
 export default defineConfig({
-  dynamicImport: {},
+  dynamicImport: {
+    loading: 'components/Loading'
+  },
   nodeModulesTransform: {
     type: 'none',
   },
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-17 18:10:58
+ * @LastEditTime: 2021-12-17 18:13:05
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \blog-app\src\components\Loading\index.tsx
+ */
+
+import React, {FC} from 'react';
+
+import styles from './index.less';
+
+const Loading: FC<any> = (props) => {
+  return (
+    <p>aaaaa</p>
+  )
+}
+
+export default Loading;
