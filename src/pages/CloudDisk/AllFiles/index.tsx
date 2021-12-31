afaf11dd1ac4ef3125ef6cf355ee70a684107580
zhangjicheng <zhangjichengcc@163.com>
2021-12-31 19:40:59 +0800

    update

--- src/pages/CloudDisk/AllFiles/index.tsx
+++ src/pages/CloudDisk/AllFiles/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-23 20:19:17
- * @LastEditTime: 2021-12-30 19:11:59
+ * @LastEditTime: 2021-12-31 19:39:26
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\pages\CloudDisk\AllFiles\index.tsx
@@ -58,6 +58,20 @@ const data: DataType[] = [
   {
     id: '1',
     name: '我的收藏',
+    size: 10249,
+    type: 'directory',
+    createTime: '2021/12/29 12:00:00',
+  },
+  {
+    id: 'resources',
+    name: '我的资源',
+    size: 10249,
+    type: 'directory',
+    createTime: '2021/12/29 12:00:00',
+  },
+  {
+    id: 'download',
+    name: '我的下载',
     size: 10249,
     type: 'directory',
     createTime: '2021/12/29 12:00:00',
--- src/pages/CloudDisk/components/HistoryBar/index.tsx
+++ src/pages/CloudDisk/components/HistoryBar/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-23 20:38:14
- * @LastEditTime: 2021-12-30 18:58:51
+ * @LastEditTime: 2021-12-31 10:33:08
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\pages\CloudDisk\components\HistoryBar\index.tsx
@@ -63,7 +63,7 @@ const HistoryBar: FC<HistoryBarProps> = (props): ReactElement => {
   async function forward() {
     if (!rightActive) return;
     if (typeof onChange === 'function') {
-      onChange(history[currentIdx + 1]);
+      await onChange(history[currentIdx + 1]);
     }
     go(1);
   }
