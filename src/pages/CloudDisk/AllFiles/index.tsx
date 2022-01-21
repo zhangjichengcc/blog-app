0ea12e9c196bbb8fcb6f57178a573e988e224db2
zhangjicheng <zhangjichengcc@163.com>
2022-01-21 11:45:39 +0800

    change env

--- src/pages/CloudDisk/AllFiles/index.tsx
+++ src/pages/CloudDisk/AllFiles/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-23 20:19:17
- * @LastEditTime: 2022-01-20 23:06:30
+ * @LastEditTime: 2022-01-21 11:25:02
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\pages\CloudDisk\AllFiles\index.tsx
@@ -234,6 +234,7 @@ const AllFiles: FC<any> = (props) => {
           onDoubleClick={openDir}
           onDelete={onFileDelete}
           onNew={onFileNew}
+          onOpen={openDir}
           loading={loading}
           onNameOk={onNameOk}
         />
