4762af4b509d914fc76a54c5dcbdf67bc33e917a
zhangjicheng <zhangjichengcc@163.com>
2022-03-09 17:12:28 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:52:20
- * @LastEditTime: 2022-03-09 16:35:08
+ * @LastEditTime: 2022-03-09 17:08:57
  * @LastEditors: zhangjicheng
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\app.ts
@@ -16,23 +16,16 @@ export function onRouteChange({ location, routes, action }: any) {
 }
 console.log('ResizeEventEm', ResizeEventEm);
 
-// // 窗口变化触发
-// function onClientResize(this: Window, ev: UIEvent) {
-//   // 触发监听类中的 clientResize 事件
-//   ResizeEventEm.emit('clientResize');
-// }
+// 窗口变化触发
+function onClientResize(this: Window, ev: UIEvent) {
+  // 触发监听类中的 clientResize 事件
+  ResizeEventEm.emit('clientResize');
+}
 
-// // 页面首次加载触发一次resize
-// ResizeEventEm.emit('clientResize');
+window.onload = () => {
+  // 可视化窗口变化 监听类
+  window.addEventListener('resize', onClientResize);
 
-// window.onload = () => {
-//   // 可视化窗口变化 监听类
-//   window.addEventListener('resize', onClientResize);
-// };
-
-// export {
-//   /**
-//    * 可视窗口变化监听类
-//    */
-//   ResizeEventEm,
-// };
+  // 页面首次加载触发一次resize
+  ResizeEventEm.emit('clientResize');
+};
