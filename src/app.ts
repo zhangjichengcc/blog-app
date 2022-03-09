ceb83ecf7bd3b00b012e45188022574a761bf97d
zhangjicheng <zhangjichengcc@163.com>
2022-03-09 12:36:24 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:52:20
- * @LastEditTime: 2022-02-28 18:39:12
+ * @LastEditTime: 2022-03-09 12:35:52
  * @LastEditors: zhangjicheng
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\app.ts
@@ -14,16 +14,16 @@ import EventEmitter from 'utils/eventEmitter';
 export function onRouteChange({ location, routes, action }: any) {
   // console.log(location.pathname);
 }
-// 可视化窗口变化 监听类
 const ResizeEventEm = new EventEmitter();
 
-// 窗口变化触发
-function onClientResize(this: Window, ev: UIEvent) {
-  // 触发监听类中的 clientResize 事件
-  ResizeEventEm.emit('clientResize');
-}
-
 window.onload = () => {
+  // 可视化窗口变化 监听类
+
+  // 窗口变化触发
+  function onClientResize(this: Window, ev: UIEvent) {
+    // 触发监听类中的 clientResize 事件
+    ResizeEventEm.emit('clientResize');
+  }
   // 添加监听事件，窗口变化触发 onClientResize
   window.addEventListener('resize', onClientResize);
   // 页面首次加载触发一次resize
