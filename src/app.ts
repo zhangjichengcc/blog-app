a90e23223485fdd922c3462d20a640d07937e374
zhangjicheng <zhangjichengcc@163.com>
2022-03-09 12:49:21 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:52:20
- * @LastEditTime: 2022-03-09 12:43:34
+ * @LastEditTime: 2022-03-09 12:49:04
  * @LastEditors: zhangjicheng
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog-app\src\app.ts
@@ -18,19 +18,19 @@ const ResizeEventEm = new EventEmitter();
 
 console.log('ResizeEventEm', ResizeEventEm);
 
-// 窗口变化触发
-function onClientResize(this: Window, ev: UIEvent) {
-  // 触发监听类中的 clientResize 事件
-  ResizeEventEm.emit('clientResize');
-}
+// // 窗口变化触发
+// function onClientResize(this: Window, ev: UIEvent) {
+//   // 触发监听类中的 clientResize 事件
+//   ResizeEventEm.emit('clientResize');
+// }
 
-// 页面首次加载触发一次resize
-ResizeEventEm.emit('clientResize');
+// // 页面首次加载触发一次resize
+// ResizeEventEm.emit('clientResize');
 
-window.onload = () => {
-  // 可视化窗口变化 监听类
-  window.addEventListener('resize', onClientResize);
-};
+// window.onload = () => {
+//   // 可视化窗口变化 监听类
+//   window.addEventListener('resize', onClientResize);
+// };
 
 export {
   /**
