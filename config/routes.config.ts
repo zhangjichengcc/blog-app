cfdb2cdc7058feedd949838bf9c6bec3f13025e7
zhangjicheng <zhangjichengcc@163.com>
2022-08-26 20:41:29 +0800

    update

--- config/routes.config.ts
+++ config/routes.config.ts
@@ -1,34 +1,34 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 15:46:56
- * @LastEditTime: 2022-03-03 10:35:33
+ * @LastEditTime: 2022-08-25 14:44:35
  * @LastEditors: zhangjicheng
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \blog-app\config\routes.config.ts
+ * @FilePath: \blog5.0_front-end\config\routes.config.ts
  */
 
 export default [
   // { path: '/', component: './index', title: 'aaaa'},
-  {
-    path: '/user',
-    component: '@/layouts/LoginLayout',
-    routes: [
-      { path: '/user/login', component: './User/Login' },
-      { path: '/login/redirect', component: './User/Login/Redirect' },
-      { path: '/user/register', component: './User/Register' },
-    ],
-  },
+  // {
+  //   path: '/user',
+  //   component: '@/layouts/LoginLayout',
+  //   routes: [
+  //     { path: '/user/login', component: './User/Login' },
+  //     { path: '/login/redirect', component: './User/Login/Redirect' },
+  //     { path: '/user/register', component: './User/Register' },
+  //   ],
+  // },
   {
     path: '/',
     component: '@/layouts/BasicLayout',
     routes: [
       { path: '/', component: './Home', title: 'home' },
-      { path: '/CloudDisk', component: './CloudDisk', title: 'CloudDisk' },
-      {
-        path: '/tools',
-        title: 'tools',
-        routes: [{ path: '/img-compress', title: 'imgCompress' }],
-      },
+      // { path: '/CloudDisk', component: './CloudDisk', title: 'CloudDisk' },
+      // {
+      //   path: '/tools',
+      //   title: 'tools',
+      //   routes: [{ path: '/img-compress', title: 'imgCompress' }],
+      // },
       { path: '/404', component: '404' },
     ],
   },
