1a0f45d296a11470a5ad67b4d8af72658253d578
zhangjicheng <zhangjichengcc@163.com>
2022-10-14 19:11:43 +0800

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,8 @@
  * @Author: zhangjicheng
  * @Date: 2022-10-14 10:36:42
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2022-10-14 17:29:22
- * @FilePath: \blog5.0_front-end\src\store\store.ts
+ * @LastEditTime: 2022-10-14 19:11:19
+ * @FilePath: \blog5.0_front-end\src\store\index.ts
  */
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
@@ -13,7 +13,9 @@ const store = configureStore({
   reducer: rootReducer,
 })
 
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch: () => AppDispatch = useDispatch
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
