4ddba1d43116bdd9db459116647454c3cea5755e
zhangjicheng <zhangjichengcc@163.com>
2022-10-18 19:04:45 +0800

    update

--- src/components/Header/PcHeader/index.tsx
+++ src/components/Header/PcHeader/index.tsx
@@ -2,21 +2,59 @@
  * @Author: zhangjicheng
  * @Date: 2022-10-12 15:47:48
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2022-10-14 10:22:14
+ * @LastEditTime: 2022-10-18 16:23:07
  * @FilePath: \blog5.0_front-end\src\components\Header\PcHeader\index.tsx
  */
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import Logo from '@/components/Logo';
 import Menu, { MenuItem } from './Menu';
 import classnames from 'classnames';
 import { useScroll } from 'ahooks';
+import tweenFunctions from 'tween-functions';
 // import { throttle } from '@/utils/utils';
 // import { useThrottle } from '@/hooks';
 
 import styles from './index.less';
 
+let top = 0;
+
 interface Props {
   menu: Array<MenuItem>
+}
+
+class Scroller {
+
+  element: HTMLElement | Window;
+  easing: string;
+  scrollId: number | undefined;
+  target: number;
+  prevTimestamp: number;
+
+  constructor(element?: HTMLElement) {
+    this.scrollId;
+    this.element = element || window;
+    this.easing = 'easeOutCubic';
+    this.prevTimestamp = 0;
+    this.target = 0;
+  }
+
+  step(timestamp: number) {
+    if (!this.prevTimestamp) this.prevTimestamp = timestamp; 
+    const currentTime = timestamp - this.prevTimestamp;
+    const value = tweenFunctions.easeInQuad(currentTime, 0, this.target!, 300 );
+    // this.prevTimestamp = timestamp;
+    console.log(value)
+    this.element.scrollTo(0, value);
+    if (value < this.target) {
+      this.scrollId = requestAnimationFrame(this.step.bind(this));
+    }
+  }
+
+  scrollTo(y: number) {
+    this.prevTimestamp = 0;
+    this.target = y;
+    this.scrollId = requestAnimationFrame(this.step.bind(this));
+  }
 }
 
 const Header: FC<Props> = (props) => {
@@ -26,10 +64,23 @@ const Header: FC<Props> = (props) => {
   } = props;
 
   const scroll = useScroll();
+  const scroller = useRef();
 
   // useEffect(function() {
   //   run();
   // }, [scroll?.top])
+
+  function scrollFn() {
+    // const dom = window;
+    // window.scrollTo(0, top);
+    // top += 10;
+    // window.requestAnimationFrame(scrollFn);
+    scroller.current.scrollTo(700)
+  }
+
+  useEffect(() => {
+    scroller.current = new Scroller();
+  })
 
   return (
     <div className={classnames({[styles.Header]: true, [styles.active]: scroll?.top && scroll?.top > 200})}>
@@ -37,7 +88,7 @@ const Header: FC<Props> = (props) => {
         <Logo />
         <div className={styles.rightContent}>
           <Menu menu={menu} />
-          <button>Download CV</button>
+          <button onClick={scrollFn}>Download CV</button>
         </div>
       </div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2022-03-02 23:05:49
- * @LastEditTime: 2022-10-17 19:01:00
+ * @LastEditTime: 2022-10-18 10:12:52
  * @LastEditors: zhangjicheng
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \blog5.0_front-end\src\pages\Home\index.tsx
  */
 
-import React, { FC, useEffect, useRef, ForwardedRef, createRef } from 'react';
+import { FC, useEffect, useRef, ForwardedRef, createRef } from 'react';
 import { useAppDispatch } from '@/store';
 import { setMenu } from '@/store/features/home/homeSlice';
 import Banner from './Banner';
@@ -19,39 +19,46 @@ import Blog from './Blog';
 
 import styles from './index.less';
 
-function getDomRect<HTMLElement>(dom): any {
+type DomRect = {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  top: number,
+  left: number,
+  bottom: number,
+  right: number,
+}
 
+/** 获取DOMRect */
+function getDomRect(dom?: HTMLElement | null): DomRect | Record<string, never> {
+  if (!dom) return {};
+  const {x, y, width, height, top, left, bottom, right} = dom.getBoundingClientRect();
+  return {
+    x, y, width, height, top, left, bottom, right
+  }
 }
 
 const Home: FC = () => {
-  const bannerDom = useRef<HTMLDivElement>();
-  const aboutDom = useRef<HTMLDivElement>();
-  const portfolioDom = useRef<HTMLDivElement>();
+  const bannerDom = useRef<HTMLDivElement>(null);
+  const aboutDom = useRef<HTMLDivElement>(null);
+  const portfolioDom = useRef<HTMLDivElement>(null);
   const serviceDom = useRef<HTMLDivElement>(null);
-  const contactDom = useRef<HTMLDivElement>();
-  const blogDom = useRef<HTMLDivElement>();
+  const contactDom = useRef<HTMLDivElement>(null);
+  const blogDom = useRef<HTMLDivElement>(null);
 
   const appDispatch = useAppDispatch();
 
-  // appDispatch(setMenu([
-  //   {label: 'Home', dom: bannerDom.current},
-  //   {label: 'About', dom: aboutDom.current},
-  //   {label: 'Portfolio'},
-  //   {label: 'Service'},
-  //   {label: 'Contact'},
-  //   {label: 'Blog'},
-  // ]))
-
   useEffect(() => {
 
-    globalThis.dom = aboutDom.current;
+    console.log(888)
     appDispatch(setMenu([
-      {label: 'Home', DOMRect: bannerDom.current?.getBoundingClientRect()},
-      {label: 'About', DOMRect: bannerDom.current?.getBoundingClientRect()},
-      {label: 'Portfolio', DOMRect: bannerDom.current?.getBoundingClientRect()},
-      {label: 'Service', DOMRect: bannerDom.current?.getBoundingClientRect()},
-      {label: 'Contact', DOMRect: bannerDom.current?.getBoundingClientRect()},
-      {label: 'Blog', DOMRect: bannerDom.current?.getBoundingClientRect()},
+      {label: 'Home', domRect: getDomRect(bannerDom.current)},
+      {label: 'About', domRect: getDomRect(aboutDom.current)},
+      {label: 'Portfolio', domRect: getDomRect(portfolioDom.current)},
+      {label: 'Service', domRect: getDomRect(serviceDom.current)},
+      {label: 'Contact', domRect: getDomRect(contactDom.current)},
+      {label: 'Blog', domRect: getDomRect(blogDom.current)},
     ]))
   }, [bannerDom.current])
 
